d1d378d926422278da888121bcd7d3a234c71fbc
Spike01011 <94932959+Spike01011@users.noreply.github.com>
2022-09-26 21:31:41 +0300

    Added backend solution

---
+++ Cheers-backend/Cheers/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Cheers.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Cheers.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return Ok("Index");
+        }
+
+        public IActionResult Privacy()
+        {
+            return Ok("Privacy");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
