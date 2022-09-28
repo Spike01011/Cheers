e3041855f13e153c9b13cb4401ad2816760e5714
Spike01011 <94932959+Spike01011@users.noreply.github.com>
2022-09-28 12:13:04 +0300

    fixed typo in DB and changed the SQL server name and database name

---
+++ Cheers-backend/Cheers/Data/Migrations/20220928091138_fixed-typo.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cheers.Data.Migrations
+{
+    public partial class fixedtypo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ShopDescription",
+                table: "Ideas",
+                newName: "ShortDescription");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ShortDescription",
+                table: "Ideas",
+                newName: "ShopDescription");
+        }
+    }
+}
--- Cheers-backend/Cheers/Models/Idea.cs
+++ Cheers-backend/Cheers/Models/Idea.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public string ShopDescription { get; set; }
+        public string ShortDescription { get; set; }
         public Category Category { get; set; }
     }
 }
