805d30731542df62f65564b4734f8a461e93c6af
BalanC21 <cristianbalan2021@gmail.com>
2022-11-07 14:00:51 +0200

    Save

---
+++ Cheers-backend/Cheers/Utils/Statics.cs
@@ -0,0 +1,15 @@
+﻿namespace Cheers.Utils
+{
+    public static class Statics
+    {
+        public static string GetEmaiSignUpBody()
+        {
+            return "<h1>Go to Cheers.com</h1></br><a href='http://localhost:3000/'>Go to the Site</a>";
+        }
+
+        public static string GetEmailSignUpSubject()
+        {
+            return "SignUp Cheers Confirmation Successed";
+        }
+    }
+}
