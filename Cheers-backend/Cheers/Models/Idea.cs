3533e5e680e83d462f3e642af6c23f47fab35f29
BalanC21 <cristianbalan2021@gmail.com>
2022-11-11 16:58:38 +0200

    Save

--- Cheers-backend/Cheers/Models/Idea.cs
+++ Cheers-backend/Cheers/Models/Idea.cs
@@ -1,32 +1,25 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Security.Cryptography;
 
 namespace Cheers.Models
 {
     public class Idea
     {
         public int Id { get; set; }
-        
         [Required(ErrorMessage = "Please enter a name for your Idea!")]
         public string? Name { get; set; }
         public ApplicationUser? Author { get; set; }
-
         [Required(ErrorMessage = "Please enter a description for your Idea!")]
         public string? Description { get; set; }
-        
         [Required(ErrorMessage = "Please enter a short description for your Idea!")]
         public string? ShortDescription { get; set; }
-        
         [Required(ErrorMessage = "Please select a Category for your Idea!")]
-        public int CategoryId { get; set; }
-
+        public int? CategoryId { get; set; }
         public Category? Category { get; set; }
-
         public int Target { get; set; }
         [NotMapped]
         public List<ImageCl>? Images { get; set; }
-        [NotMapped] public string? Email { get; set; }
+        [NotMapped, EmailAddress] public string? Email { get; set; }
 
         public override string ToString()
         {
